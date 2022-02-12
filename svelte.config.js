ea5c69d79d4bbc6f8c4be8c5c28db0e961637c43
Valery Shvargonov <animekazna@gmail.com>
2022-02-13 01:21:10 +0700

    configere build to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,31 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			// default options are shown
-			out: 'build',
-			precompress: false,
-			env: {
-				path: 'SOCKET_PATH',
-				host: 'HOST',
-				port: 'PORT',
-				origin: 'ORIGIN',
-				headers: {
-					protocol: 'PROTOCOL_HEADER',
-					host: 'HOST_HEADER'
-				}
-			}
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false
 		})
+		// adapter: adapter({
+		// 	// default options are shown
+		// 	out: 'build',
+		// 	precompress: false,
+		// 	env: {
+		// 		path: 'SOCKET_PATH',
+		// 		host: 'HOST',
+		// 		port: 'PORT',
+		// 		origin: 'ORIGIN',
+		// 		headers: {
+		// 			protocol: 'PROTOCOL_HEADER',
+		// 			host: 'HOST_HEADER'
+		// 		}
+		// 	}
+		// })
 	}
 };
 
