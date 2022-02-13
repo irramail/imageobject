57c00a584949fcab12f592d11be6a7b0ce17ec31
Valery Shvargonov <animekazna@gmail.com>
2022-02-13 13:18:52 +0700

    Reconfigure adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,23 +8,8 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html',
-			precompress: false
+			precompress: true
 		})
-		// adapter: adapter({
-		// 	// default options are shown
-		// 	out: 'build',
-		// 	precompress: false,
-		// 	env: {
-		// 		path: 'SOCKET_PATH',
-		// 		host: 'HOST',
-		// 		port: 'PORT',
-		// 		origin: 'ORIGIN',
-		// 		headers: {
-		// 			protocol: 'PROTOCOL_HEADER',
-		// 			host: 'HOST_HEADER'
-		// 		}
-		// 	}
-		// })
 	}
 };
 
